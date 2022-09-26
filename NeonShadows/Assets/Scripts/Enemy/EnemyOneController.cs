29c6d1b4f56c0988ea691399017caf7929eaa20d
Aldo Degollado <aldodegollado@hotmail.com>
2022-09-26 14:43:17 -0500

    Adds enemy that patrols and chases the player
    Creates and configures a navmesh for the patrolling enemy implementation. Few changes to the enemy that only chases.

--- NeonShadows/Assets/Scripts/Enemy/EnemyOneController.cs
+++ NeonShadows/Assets/Scripts/Enemy/EnemyOneController.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyOne : MonoBehaviour
+public class EnemyOneController : MonoBehaviour
 {
 
     public float rangeAlert;
@@ -10,6 +11,8 @@ public class EnemyOne : MonoBehaviour
     public bool stayActive;
     public Transform player;
     public float speed;
+    //public static event Action<Enemy> OnEnemyKilled;
+    //public float gravity = 9.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +31,8 @@ public class EnemyOne : MonoBehaviour
 
             // Para que solamente mueva la mirada en Y
             Vector3 playerPos = new Vector3(player.position.x, player.position.y, player.position.z);
-            transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
-            transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
-
-            
+            transform.LookAt(playerPos);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.position.x, transform.position.y, player.position.z), speed * Time.deltaTime);
         }
     }
 
