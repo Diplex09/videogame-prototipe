de22a72a7800ba4ee1999fe1c5bde939bc414436
Diego Velázquez Moreno <diegoa1962@hotmail.com>
2022-09-28 21:26:05 -0500

    Health system
    Made the health system with a constructor and capacities to receive damage or heal

---
+++ NeonShadows/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    private int health;
+
+    public HealthSystem(int health) {
+        this.health = health;
+    }
+
+    public int GetHealth(){
+        return health;
+    }
+
+    public void Damage(int damageAmount){
+        health = health - damageAmount; 
+    }
+
+    public void Heal(int healAmount){
+        health = health + healAmount;
+    }
+
+}
