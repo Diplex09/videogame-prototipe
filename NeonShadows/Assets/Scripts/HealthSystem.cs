26f53f830403aed0e2ccba7237a08cd93ac077a2
Diego Velázquez Moreno <diegoa1962@hotmail.com>
2022-09-30 18:58:14 -0500

    Update HealthSystem.cs
    Added a way to fix health status and dont get negative health

--- NeonShadows/Assets/Scripts/HealthSystem.cs
+++ NeonShadows/Assets/Scripts/HealthSystem.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     private int health;
+    private int healthMax
 
-    public HealthSystem(int health) {
-        this.health = health;
+    public HealthSystem(int healthMax) {
+        this.healthMax = healthMax;
+        health = healthMax
     }
 
     public int GetHealth(){
@@ -15,11 +17,18 @@ public class HealthSystem : MonoBehaviour
     }
 
     public void Damage(int damageAmount){
+        
         health = health - damageAmount; 
+        if health <= 0 {
+            health = 0 ;
+        }
     }
 
     public void Heal(int healAmount){
         health = health + healAmount;
+        if health > healthMax {
+            health = healthMax
+        }
     }
 
 }
