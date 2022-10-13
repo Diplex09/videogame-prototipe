8ed25227aa89bbef6391e770de0485a133c7ec5f
David12081 <A01706054@itesm.mx>
2022-10-12 19:36:17 -0500

    Player Attack (kinda works)

--- NeonShadows/Assets/Scripts/Player/PlayerAttack.cs
+++ NeonShadows/Assets/Scripts/Player/PlayerAttack.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [SerializeField] Animator _anim;
-    [SerializeField] float _coolDownTime;
-    float _nextFireTime = 0f;
+    [SerializeField] private Animator _anim;
+    [SerializeField] private float _cooldownTime = 0.8f;
+    private float _nextFireTime = 0f;
     public static int _noOfClicks = 0;
-    float _lastClickedTime = 0f;
-    float _maxComboDelay = 1f;
-    float _timeBetweenHits = 0.7f;
+    float _lastClickedTime = 0;
+    float _maxComboDelay = 1;
 
-    private void Update()
+    void Update()
     {
-        if(_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > _timeBetweenHits && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
+
+        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
         {
-            _anim.SetBool("hit1",false);
+            _anim.SetBool("hit1", false);
         }
-        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > _timeBetweenHits && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
+        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
         {
             _anim.SetBool("hit2", false);
         }
-        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > _timeBetweenHits && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit3"))
+        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit3"))
         {
             _anim.SetBool("hit3", false);
             _noOfClicks = 0;
         }
 
-        if(Time.time - _lastClickedTime > _maxComboDelay)
+
+        if (Time.time - _lastClickedTime > _maxComboDelay)
         {
             _noOfClicks = 0;
         }
-        if(Time.time > _nextFireTime)
+
+        //cooldown time
+        if (Time.time > _nextFireTime)
         {
-            if(Input.GetMouseButtonDown(0))
+            // Check for mouse input
+            if (Input.GetMouseButtonDown(0))
             {
                 OnClick();
             }
@@ -43,21 +47,21 @@ public class PlayerAttack : MonoBehaviour
 
     void OnClick()
     {
+        //so it looks at how many clicks have been made and if one animation has finished playing starts another one.
         _lastClickedTime = Time.time;
         _noOfClicks++;
-        if(_noOfClicks == 1)
+        if (_noOfClicks == 1)
         {
             _anim.SetBool("hit1", true);
         }
         _noOfClicks = Mathf.Clamp(_noOfClicks, 0, 3);
 
-        if(_noOfClicks >= 2 && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > _timeBetweenHits && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
+        if (_noOfClicks >= 2 && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
         {
             _anim.SetBool("hit1", false);
             _anim.SetBool("hit2", true);
         }
-
-        if (_noOfClicks >= 3 && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > _timeBetweenHits && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
+        if (_noOfClicks >= 3 && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
         {
             _anim.SetBool("hit2", false);
             _anim.SetBool("hit3", true);
