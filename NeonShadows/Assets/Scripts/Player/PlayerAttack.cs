b7e541d3ebde37332fcb933eea222ee3d8d0a1b4
David12081 <A01706054@itesm.mx>
2022-10-12 20:07:08 -0500

    Player Attack (works better)

--- NeonShadows/Assets/Scripts/Player/PlayerAttack.cs
+++ NeonShadows/Assets/Scripts/Player/PlayerAttack.cs
@@ -5,66 +5,35 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
-    [SerializeField] private float _cooldownTime = 0.8f;
-    private float _nextFireTime = 0f;
-    public static int _noOfClicks = 0;
-    float _lastClickedTime = 0;
-    float _maxComboDelay = 1;
+    private int _combo;
+    private bool _attacking;
 
-    void Update()
+    private void Update()
     {
+        Combos();
+    }
 
-        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
+    void Combos()
+    {
+        if (Input.GetMouseButtonDown(0) && !_attacking)
         {
-            _anim.SetBool("hit1", false);
-        }
-        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
-        {
-            _anim.SetBool("hit2", false);
-        }
-        if (_anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit3"))
-        {
-            _anim.SetBool("hit3", false);
-            _noOfClicks = 0;
-        }
-
-
-        if (Time.time - _lastClickedTime > _maxComboDelay)
-        {
-            _noOfClicks = 0;
-        }
-
-        //cooldown time
-        if (Time.time > _nextFireTime)
-        {
-            // Check for mouse input
-            if (Input.GetMouseButtonDown(0))
-            {
-                OnClick();
-            }
+            _attacking = true;
+            _anim.SetTrigger("" + _combo);
         }
     }
 
-    void OnClick()
+    public void StartCombo()
     {
-        //so it looks at how many clicks have been made and if one animation has finished playing starts another one.
-        _lastClickedTime = Time.time;
-        _noOfClicks++;
-        if (_noOfClicks == 1)
+        _attacking = false;
+        if(_combo < 3)
         {
-            _anim.SetBool("hit1", true);
+            _combo++;
         }
-        _noOfClicks = Mathf.Clamp(_noOfClicks, 0, 3);
+    }
 
-        if (_noOfClicks >= 2 && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit1"))
-        {
-            _anim.SetBool("hit1", false);
-            _anim.SetBool("hit2", true);
-        }
-        if (_noOfClicks >= 3 && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && _anim.GetCurrentAnimatorStateInfo(0).IsName("hit2"))
-        {
-            _anim.SetBool("hit2", false);
-            _anim.SetBool("hit3", true);
-        }
+    public void FinishAnimation()
+    {
+        _attacking = false;
+        _combo = 0;
     }
 }
